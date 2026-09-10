c6cfc7c8362a4b3c95b1caf4eb1f37d268f7d283
nat-30102000 <akashvenkat3010@gmail.com>
2026-09-11 00:05:02 +0530

    feat: add js/app.js (minimal probe then replace)

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-PLACEHOLDER_WILL_FAIL
+/**
+ * staging-bug-pack: Main Application Logic
+ * Pure client-side, zero dependencies
+ */
+
+(function () {
+  'use strict';
+
+  // State
+  var state = {
+    title: '',
+    stagingUrl: '',
+    browser: '',
+    os: '',
+    testAccountNotes: '',
+    steps: [''],
+    expected: '',
+    actual: '',
+    screenshots: [],
+    touched: {},
+    activePreviewTab: 'raw'
+  };
+
+  console.log('staging-bug-pack app bootstrap');
+})();
